50a9cab37095c1f69ca96a0bb0133848cb05a579
erolbe1 <erolbe1@corp.onespan.com>
2020-03-30 20:44:19 -0400

    ngFor directive with Moment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-servers',
@@ -11,6 +12,11 @@ export class ServersComponent implements OnInit {
   serverName='';
   userName='';
   serverCreated= false;
+  servers = ['Testserver', 'Testserver 2'];
+  showDetails = false;
+  clickCount = [];
+  now = moment();
+  
 
   constructor() { 
     setTimeout(() => {
@@ -23,12 +29,23 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated= true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server created! Name is ' + this.serverName + '.';
     this.serverName = '';
   }
 
   onReset() {
     this.userName = (<HTMLInputElement>event.target).value;
+  }
+
+  onShowDetails() {
+    let current_timestamp = moment(new Date()).format("dddd, MMMM Do YYYY, h:mm:ss a");
+    this.showDetails = true;
+    this.clickCount.push(current_timestamp);
+  }
+
+  getShowDetails() {
+    return this.showDetails;
   }
 
   onUpdateServerName(event: Event) {
